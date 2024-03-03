aa778771eaf6cbc3eeae8c4d79dacab7a1e062c2
vvshadow <zayd.benoumeur@gmail.com>
2024-03-03 20:20:20 +0100

    remise en forme

--- bibliotheque/controleur/controleurPrincipal.php
+++ bibliotheque/controleur/controleurPrincipal.php
@@ -5,6 +5,7 @@ function controleurPrincipal($action){
     $lesActions["defaut"] = "accueil.php";
     $lesActions["chercher"] = "chercher.php";
     $lesActions["contact"] = "contact.php";
+    $lesActions["connexion"] = "connexion.php";
     
     if (array_key_exists ( $action , $lesActions )){
         return $lesActions[$action];
