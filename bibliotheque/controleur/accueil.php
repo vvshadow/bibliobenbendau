f5198f4052917e2a3fd6a66df2222626ec6ae3eb
Hakho05 <146886482+Hakho05@users.noreply.github.com146886482+Hakho05@users.noreply.github.com>
2024-03-06 12:42:38 +0100

    test

--- bibliotheque/controleur/accueil.php
+++ bibliotheque/controleur/accueil.php
@@ -1,13 +1,9 @@
 <?php
-
-//  Partie d'appel au modèle si besoin 
-
-
-// Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
-
-
-
-
-// appel du script de vue qui permet de gerer l'affichage des donnees
 include "/vue/vueAccueil.php";
+
+$marequete = "SELECT * FROM livre";
+$req = $bdd->query($marequete);
+$rep = pg_fetch_all($req);
+
+var_dump($rep);
 ?>
--- bibliotheque/vue/vueAccueil.php
+++ bibliotheque/vue/vueAccueil.php
@@ -1,7 +1,10 @@
 <?php
-include("../inc/header.inc");
+include "inc/headr.inc";
 ?>
         <h2>Voici la liste des livres disponibles :</h2>
     </body>
 
 </html>
+<?php
+include "inc/footer.inc";
+?>
