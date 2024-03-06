f14d23dadf2f223c5c50ffccf458d9b1ee64d613
vvshadow <zayd.benoumeur@gmail.com>
2024-03-06 20:18:39 +0100

    push de vue accueil

--- bibliotheque/vue/vueAccueil.php
+++ bibliotheque/vue/vueAccueil.php
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-<?php
-include "inc/headr.inc";
-?>
-        <h2>Voici la liste des livres disponibles :</h2>
-    </body>
 
-</html>
-<?php
-include "inc/footer.inc";
-?>
-=======
         <h2>Voici la liste des livres disponibles :</h2>
->>>>>>> 2ddc3ff0d066e48ea95bc141f6d5a91e9714fa03
