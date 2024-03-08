a0a2339753084da15287e5aae31441eed421780e
vvshadow <zayd.benoumeur@gmail.com>
2024-03-08 14:05:44 +0100

    profil.php

--- bibliotheque/controleur/profil.php
+++ bibliotheque/controleur/profil.php
@@ -6,7 +6,16 @@ include 'includes/db.php';
 
 
 // Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
+if(isset($_SESSION['d']))
+{
 
+
+    $requser = $bdd->prepare('SELECT * FROM user WHERE d = ?');
+    $requser->execute(array($_SESSION['d']));
+    $userinfo = $requser->fetch();
+} else {
+    header("location: index.php");
+}
 
 
 
