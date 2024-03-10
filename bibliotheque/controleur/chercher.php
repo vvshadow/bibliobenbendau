76aa4ffd76b0cbbf77bd2eca62d721bc58cca87a
vvshadow <zayd.benoumeur@gmail.com>
2024-03-10 17:20:31 +0100

    partie kyllian

--- bibliotheque/controleur/chercher.php
+++ bibliotheque/controleur/chercher.php
@@ -1,10 +1,57 @@
 <?php
 
 //  Partie d'appel au modèle si besoin 
-
+include "includes/db.php";
 
 // Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
 
+
+
+
+//requete Livres
+$requiser = $bdd->prepare('SELECT * FROM Livre');
+$donneesLivres = $bdd->query($requiser);
+$selectLivre = $donneesLivres->fetch();
+
+//requetes codeLivres
+$codeLivre = $bdd->prepare('SELECT * FROM codelivre');
+$donneesCodes = $bdd->query($codeLivre);
+$selectCode = $donneesCodes->fetch();
+
+//requetes titre
+$codeTitre = $bdd->prepare('SELECT * FROM titre');
+$donnesTitre = $bdd->query($codeTitre);
+$selectTitre = $donnesTitre->fetch();
+
+//requetes Resume
+$codeResume = $bdd->prepare('SELECT * FROM resume');
+$donneesResume = $bdd->query($donneesResume);
+$selectResume = $donneesResume->fetch();
+
+//requetes auteurs
+$codeAuteur = $bdd->prepare('SELECT* FROM auteur');
+$donneesAuteur = $bdd->query($donneesAuteur);
+$selectAuteur = $donneesAuteur->fetch();
+
+//requet Code
+$codeID = $bdd->prepare('SELECT codelivre FROM livre');
+$donneesId = $bdd->query($codeID);
+$selectId = $donneesId->fetch();
+
+//requetes titre
+$codeTitre = $bdd->prepare('SELECT titre FROM livre');
+$donnesTitre = $bdd->query($codeTitre);
+$selectTitre = $donnesTitre->fetch();
+
+//requetes Resume
+$codeResume = $bdd->prepare('SELECT resume FROM livre');
+$donneesResume = $bdd->query($donneesResume);
+$selectResume = $donneesResume->fetch();
+
+//requetes auteurs
+$codeAuteur = $bdd->prepare('SELECT auteur FROM livre');
+$donneesAuteur = $bdd->query($donneesAuteur);
+$selectAuteur = $donneesAuteur->fetch();
 
 
 
--- bibliotheque/vue/vueChercher.php
+++ bibliotheque/vue/vueChercher.php
@@ -1,2 +1,287 @@
 <h1>Recherche</h1>
 <p> Ceci est la page recherche</p>
+<table border="1">
+    <caption><h3>Livres disponible dans la bibliotheque</h3></caption>
+    <thead>
+        <tr>
+            <th>Référence</th>
+            <th>Titre</th>
+            <th>Résumé</th>
+            <th>Auteur</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selecutAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+        <tr>
+            <td><?php echo $selectCode;?></td>
+            <td><?php echo $selectTitre;?></td>
+            <td><?php echo $selectResume?></td>
+            <td><?php echo $selectAuteur?></td>
+        </tr>
+    </tfoot>
+</table>
