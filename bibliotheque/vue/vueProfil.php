b390e58e5d3a7cf8c8167edda6f87fb1aa8d743a
vvshadow <65791979+vvshadow@users.noreply.github.com>
2024-03-12 14:14:05 +0100

    profil

--- bibliotheque/vue/vueProfil.php
+++ bibliotheque/vue/vueProfil.php
@@ -1,16 +1,15 @@
 <ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Mes O.B.J</a></li>
+			<li><a href="#">Mes Livres</a></li>
 			<li><a href="#">Ajoutez livre</a></li>
 			<li><a href="#">Suggestion</a></li>
                         <li><a href="#">Contact</a></li>
 		  </ul>
                   <br><br>
-<div align="center">
+
 
 
         <div class="box">
-    <div align="left">
+   
 
            <div class="image"> 
             
@@ -20,20 +19,17 @@
 
 
            
-        <div align="right">
+
     <h3>Pseudo = <?php echo $_SESSION['nom']?> </h3> 
     <h3>E-mail = <?php echo $_SESSION['mail']?></h3> 
-    <h3>Mot de passe = <?php echo $_SESSION['mdp']?></h3> 
-    <h3>Liste d'objectif = <?php echo $_SESSION['d']?></h3> <br></div>
-    <div align="center">
+    <h3>Liste des livres = <?php echo $_SESSION['d']?></h3> <br></div>
+   
     <div class="bt">
-		<a href="modif-profil.php"><button class="button" style="vertical-align:middle"><span>Modifier Le Profil</span></button></a>
+		<a href="#"><button class="button" style="vertical-align:middle"><span>Modifier Le Profil</span></button></a>
 	</div> <div class="bt">
-		<a href="deco.php"><button class="button" style="vertical-align:middle"><span>Deconnexion</span></button></a>
+		<a href="#"><button class="button" style="vertical-align:middle"><span>Deconnexion</span></button></a>
 	</div>
 
-        </div>
-</div>
-</div>
+        
 </div>
 </div><br>
