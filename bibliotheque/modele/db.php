40e9da7d79583818feaa5e77894d80e9f8a3f6f8
vvshadow <65791979+vvshadow@users.noreply.github.com>
2024-03-12 14:37:41 +0100

    ts

--- bibliotheque/modele/db.php
+++ bibliotheque/modele/db.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=10.13.216.29;dbname=login4544;charset=utf8','dblogin4544','ydtkfzeMbBxlKik');
+$bdd = new PDO('mysql:host=localhost;dbname=login4544;charset=utf8','dblogin4544','ydtkfzeMbBxlKik');
 
 
 ?>
