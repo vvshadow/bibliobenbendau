495159755e575bd2cf0ad2c796abeb3399630a7f
Hakho05 <146886482+Hakho05@users.noreply.github.com146886482+Hakho05@users.noreply.github.com>
2024-03-12 10:33:39 +0100

    test

--- bibliotheque/controleur/chercher.php
+++ bibliotheque/controleur/chercher.php
@@ -1,13 +1,29 @@
 <?php
-
-//  Partie d'appel au modèle si besoin 
-
-
-// Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
-
-
-
-
-// appel du script de vue qui permet de gerer l'affichage des donnees
 include "vue/vueChercher.php";
+include "includes/db.php";
+
+//requete Livres
+$requiser = $bdd->prepare('SELECT * FROM Livre');
+$donneesLivres = $bdd->query($requiser);
+$selectLivre = $donneesLivres->fetch();
+
+//requet Code
+$codeID = $bdd->prepare('SELECT codelivre FROM livre');
+$donneesId = $bdd->query($codeID);
+$selectId = $donneesId->fetch();
+
+//requetes titre
+$codeTitre = $bdd->prepare('SELECT titre FROM livre');
+$donnesTitre = $bdd->query($codeTitre);
+$selectTitre = $donnesTitre->fetch();
+
+//requetes Resume
+$codeResume = $bdd->prepare('SELECT resume FROM livre');
+$donneesResume = $bdd->query($donneesResume);
+$selectResume = $donneesResume->fetch();
+
+//requetes auteurs
+$codeAuteur = $bdd->prepare('SELECT auteur FROM livre');
+$donneesAuteur = $bdd->query($donneesAuteur);
+$selectAuteur = $donneesAuteur->fetch();
 ?>
