f377b113d80105a37478a2d211b49e63b8fb3b4c
vvshadow <65791979+vvshadow@users.noreply.github.com>
2024-03-12 13:41:57 +0100

    bdd

--- bibliotheque/modele/db.php
+++ bibliotheque/modele/db.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=login5260;charset=utf8','dblogin5260','rIgUUppFtMTdpFs');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=login4544;charset=utf8','dblogin4544','ydtkfzeMbBxlKik');
 
 
 ?>
