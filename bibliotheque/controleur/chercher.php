787dc042373efa5dde84257779e28160476e4e6a
vvshadow <65791979+vvshadow@users.noreply.github.com>
2024-03-25 21:59:32 +0100

    schearch

--- bibliotheque/controleur/chercher.php
+++ bibliotheque/controleur/chercher.php
@@ -1,29 +1,46 @@
 <?php
-include "modele/db.php";
+function connexionBDD()
+{
+    $bdd = 'mysql:host=10.23.216.21;dbname=bibliotheque';
+    $user = 'bibliotheque';
+    $password = 'meSdEkETU2m5i!';
+    try {
+        $ObjConnexion = new PDO(
+            $bdd,
+            $user,
+            $password,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            )
+        );
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $ObjConnexion;
 
-$serveur = 'localhost';
-$user  = 'bibliotheque';
-$mot_de_passe = 'meSdEkETU2m5i}!' ;
-
-$conn = new mySQLI ($serveur, $user, $mot_de_passe);
-
-
-
-if ($connexion->connect_error) {
-   die("Échec de la connexion à la base de données : " . $connexion->connect_error);
+}
+$alldemande = $bdd->query("SELECT * FROM users ORDER BY id DESC");
+if(isset($_GET['titre']) && !empty($_GET['genre']) && !empty($_GET['sortie']) && !empty($_GET['auteur'])) {
+   $tit = htmlspecialchars($_GET['titre']);
+   $genre = htmlspecialchars($_GET['genre']);
+   $sortie = htmlspecialchars($_GET['sortie']);
+   $auteur = htmlspecialchars($_GET['auteur']);
+   $alldemande = $bdd->query('SELECT titre , genre, annesortie FROM livre WHERE ');
 }
 
 
-$titre = $_GET['titre'];
-$genre = $_GET['genre'];
 
 
-$sql = "SELECT * FROM livres WHERE titre LIKE '%$titre%' AND genre LIKE '%$genre%'";
 
 
-$resultat = $connexion->query($sql);
+
+// ancien  code
+// $sql = "SELECT * FROM livres WHERE titre LIKE '%$titre%' AND genre LIKE '%$genre%'";
 
 
-$connexion->close();
+// $resultat = $connexion->query($sql);
+
+
+// $connexion->close();
 
 ?>
--- bibliotheque/modele/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=login4544;charset=utf8','login4544','ydtkfzeMbBxlKik');
-
-
-
-?>
