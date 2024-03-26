bc70055ec7101dcdb1f9b3b98b1e0fce3a31b419
vvshadow <65791979+vvshadow@users.noreply.github.com>
2024-03-26 13:53:20 +0100

    function  rechercher validé

--- bibliotheque/controleur/livre.php
+++ bibliotheque/controleur/livre.php
@@ -1,2 +1,32 @@
 <?php
-include vue/vueAcceuil.php
+include "vue/vueAcceuil.php";
+
+$servername = 'localhost';
+$username = 'bibliotheque';
+$password = 'meSdEkETU2m5i}!';
+
+//On établit la connexion
+$conn = new mysqli($servername, $username, $password);
+
+//On vérifie la connexion
+if($conn->connect_error){
+    die('Erreur : ' .$conn->connect_error);
+}
+echo 'Connexion réussie';
+
+$requete_detail_livre = $bdd->prepare('SELECT * From bibliotheque Where id_livre = :id');
+$requete_auteur = $bdd->prepare('SELECT * From auteur Where auteur  id_auteur = :id');
+
+if($auteur) {
+    echo "<p>Auteur : " . $auteur['nom'] . " " . $auteur['prenom'] . "</p>";
+} else {
+    echo "<p>Auteur non disponible</p>";
+}
+} else {
+echo "<p>Livre non trouvé</p>";
+}
+} else {
+echo "<p>Identifiant du livre non spécifié</p>";
+}
+
+?>
--- bibliotheque/modele/mesFonctionsAccesBDD.php
+++ bibliotheque/modele/mesFonctionsAccesBDD.php
@@ -1,12 +1,12 @@
 <?php
 function connexionBDD()
 {
-    $bdd = 'mysql:host=10.23.216.21;dbname=bibliotheque';
+    $db = 'mysql:host=10.23.216.21;dbname=bibliotheque';
     $user = 'bibliotheque';
-    $password = 'meSdEkETU2m5i!';
+    $password = 'meSdEkETU2m5i}!';
     try {
         $ObjConnexion = new PDO(
-            $bdd,
+            $db,
             $user,
             $password,
             array(
@@ -16,9 +16,49 @@ function connexionBDD()
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
-    return $ObjConnexion;
+ //   return $ObjConnexion;
 
 };
 
+function recherche($bdd, $titre, $genre, $sortie, $auteur) {
+    // Initialisation de la requête SQL
+    $sql = "SELECT * FROM livre WHERE ";
 
+    // Construction de la clause WHERE en fonction des paramètres fournis
+    $conditions = array();
+    if (!empty($titre)) {
+        $conditions[] = "titre LIKE '%$titre%'";
+    }
+    if (!empty($genre)) {
+        $conditions[] = "genre = '$genre'";
+    }
+    if (!empty($sortie)) {
+        // Conversion du format de date pour correspondre au format de la base de données
+        $sortie = date('Y-m-d', strtotime($sortie));
+        $conditions[] = "annesortie = '$sortie'";
+    }
+    if (!empty($auteur)) {
+        $conditions[] = "auteur = '$auteur'";
+    }
+
+    // Concaténation des conditions avec "AND"
+    $sql .= implode(" AND ", $conditions);
+
+    // Exécution de la requête SQL
+    $resultats = $bdd->query($sql);
+
+    // Retourner les résultats
+    return $resultats;
+}
+
+// Utilisation de la fonction recherche
+if (isset($_GET['titre']) && isset($_GET['genre']) && isset($_GET['sortie']) && isset($_GET['auteur'])) {
+    $tit = htmlspecialchars($_GET['titre']);
+    $genre = htmlspecialchars($_GET['genre']);
+    $sortie = htmlspecialchars($_GET['sortie']);
+    $auteur = htmlspecialchars($_GET['auteur']);
+
+    $resultatsRecherche = recherche($bdd, $tit, $genre, $sortie, $auteur);
+    // Utilisez $resultatsRecherche pour traiter les résultats de la recherche
+};
 ?>
